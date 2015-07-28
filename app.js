279a4c692e9ba7cf98a5984c0f36ce92b4bafe4a
johnny lamb <agnusjack@gmail.com>
2015-07-28 17:04:07 -0600

    first commit

--- app.js
+++ app.js
@@ -1,3 +1,34 @@
 $(document).ready(function() {
-	console.log("The document is ready!");
-})
+
+  $('.image-container').append("<article class='home' data-price='20000'><img src='img/myfirstcabin.jpg' /><h4>$20,000</h4></article>");
+  $('.image-container').append("<article class='home' data-price='50000'><img src='img/ontheprairy.jpg' /><h4>$50,000</h4></article>");
+  $('.image-container').append("<article class='home' data-price='200000'><img src='img/redcabin.jpg' /><h4>$200,000</h4></article>");
+
+
+
+  $('.dark').on("click", function() {
+    $('body').css('background', "url(img/dark_wood.png)");
+  });
+  $('.light').on("click", function() {
+    $('body').css('background', "url(img/tileable_wood_texture.png)");
+  });
+  $('.textures').css('width', '150px').append('<span class="forest"></span>');
+  $('.forest').css('background', "url(img/forest.jpg)");
+  $('.forest').on("click", function() {
+    $('body').css(
+    {
+      'background-size': 'cover',
+      'background': "url(img/forest.jpg)"
+        }
+      );
+  });
+
+  $('.low').on("click", function(){
+
+    console.log("you clicked")
+
+  });
+
+
+
+});
