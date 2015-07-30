a7c07099bd119f3725fc5c033ecfd75fb591f45a
johnny lamb <agnusjack@gmail.com>
2015-07-30 10:32:40 -0600

    second commit

--- app.js
+++ app.js
@@ -17,18 +17,60 @@ $(document).ready(function() {
   $('.forest').on("click", function() {
     $('body').css(
     {
-      'background-size': 'cover',
+      'background-size': 'contain',
       'background': "url(img/forest.jpg)"
         }
       );
   });
 
+
+
+
   $('.low').on("click", function(){
 
-    console.log("you clicked")
+    $('.image-container article').sort(sort_article).appendTo('.image-container');
+
+          function sort_article(a,b){
+          return ($(b).data('price')) < ($(a).data('price')) ? 1 : -1;
+        }
+
+    //   var sortedArray= [];
+    //   var homeArray = $('.home');
+    //   for (var i = 0 ; i < homeArray.length; i++){
+
+    //   var dataPrice = $('.home')[i].dataset.price;
+    //   sortedArray.push(dataPrice);
+
+    // }
+    // // sort low to hi
+    //   sortedArray.sort(function(a, b){return a - b});
+    //   console.log(sortedArray);
 
   });
 
+    $('.high').on("click", function(){
+
+    $('.image-container article').sort(sort_article).appendTo('.image-container');
+
+          function sort_article(a,b){
+          return ($(b).data('price')) > ($(a).data('price')) ? 1 : -1;
+        }
+
+
+    //   var sortedArray= [];
+    //   var homeArray = $('.home');
+    //   for (var i = 0 ; i < homeArray.length; i++){
+
+    //   var dataPrice = $('.home')[i].dataset.price;
+    //   sortedArray.push(dataPrice);
+
+    // }
+    // // sort hi to low
+    //   sortedArray.sort(function(a, b){return b - a});
+    //   console.log(homeArray);
+    //   console.log(sortedArray);
+
+  });
 
 
 });
